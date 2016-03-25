e86b31b47b23442138cc53854d976ce613a030f2
Chedwin <edwin14chen@gmail.com>
2016-03-25 17:03:08 -0400

    Controller support

---
+++ Hurricane/Hurricane-Core/Controller.cpp
@@ -0,0 +1,30 @@
+#include "Controller.h"
+#include "Debug.h"
+
+using namespace CORE;
+
+Controller::Controller()  {
+	controller = nullptr;
+	InitController();
+}
+Controller::~Controller() {
+	ShutdownContoller();
+}
+
+void Controller::InitController() {
+	for (int i = 0; i < SDL_NumJoysticks(); i++) {
+		if (SDL_IsGameController(i)) {
+			controller = SDL_GameControllerOpen(i);
+			std::cout << SDL_GameControllerMapping(controller) << std::endl;
+			break;
+		}
+	}
+	Debug::ConsoleLog("Controller has been initialized!");
+} // end InitController()
+
+void Controller::ShutdownContoller() {
+	if (controller) {
+		SDL_GameControllerClose(controller);
+		controller = nullptr;
+	}
+}
